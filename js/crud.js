2a7ed3e165f201dc11169472d711fd22b3f096b5
Jhoel Flores Alejo <joe0zf@gmail.com>
2021-10-26 09:33:11 -0500

    ADD: update and delete

--- js/crud.js
+++ js/crud.js
@@ -14,19 +14,23 @@ const get_data_localstorage = () => {
 };
 
 const set_data_localstorage = () => {
-  let data = {
-    id: tasks.length + 1,
-    title: title_task.value,
-    description: description_task.value,
-    complete: false,
-  };
-  tasks.push(data);
+  if (title_task.value && description_task.value) {
+    let data = {
+      id: tasks.length + 1,
+      title: title_task.value,
+      description: description_task.value,
+      complete: false,
+    };
+    tasks.push(data);
 
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-  print_tasks();
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    print_tasks();
 
-  title_task.value = "";
-  description_task.value = "";
+    title_task.value = "";
+    description_task.value = "";
+  } else {
+    alert("Complete todos los campos");
+  }
 };
 
 const print_tasks = () => {
@@ -44,9 +48,15 @@ const print_tasks = () => {
               <p>${e.title}</p>
             </div>
             <div class="left">
-              <img class="detail-btn" src="img/eye.svg" alt="" />
-              <img src="img/edit.svg" alt="" />
-              <img src="img/delete.svg" alt="" />
+              <img id="detail-${
+                e.id
+              }" class="detail-btn" src="img/eye.svg" alt="" />
+              <img id="edit-${
+                e.id
+              }" class="edit-btn" src="img/edit.svg" alt="" />
+              <img id="delete-${
+                e.id
+              }" class="delete-btn" src="img/delete.svg" alt="" />
             </div>
           </div>
         
@@ -63,6 +73,79 @@ const change_complete_state = (event) => {
   }
 };
 
+const view_data = (id_detail) => {
+  let res = tasks.find((e) => "detail-" + e.id == id_detail);
+  if (res) {
+    let task_detail_container = document.getElementById("task-detail");
+    task_detail_container.innerHTML = "";
+
+    task_detail_container.insertAdjacentHTML(
+      "beforeend",
+      `
+        <div class="task-detail__title">${res.title}</div>
+        <p class="task-detail__description">
+          ${res.description}
+        </p>
+        <div class="task-detail__complete">completo : ${
+          res.complete ? "SI" : "NO"
+        }</div>
+    `
+    );
+  }
+};
+
+const generate_form = (id_edit) => {
+  let res = tasks.find((e) => "edit-" + e.id == id_edit);
+  if (res) {
+    let task_detail_container = document.getElementById("task-detail");
+    task_detail_container.innerHTML = "";
+
+    task_detail_container.insertAdjacentHTML(
+      "beforeend",
+      `
+        <div class="edit-form-container">
+        <input id="title-task-edit" type="text" placeholder="Título de la tarea" value="${res.title}" />
+        <textarea
+          id="description-task-edit"
+          type="text"
+          placeholder="Descripción de la tarea"
+        >${res.description}</textarea>
+        <button id="save-btn-edit">Guardar</button>
+        </div>
+      
+    `
+    );
+    let save_btn_edit = document.getElementById("save-btn-edit");
+    save_btn_edit.addEventListener("click", () => {
+      let title_task_edit = document.getElementById("title-task-edit");
+      let description_task_edit = document.getElementById(
+        "description-task-edit"
+      );
+      if (title_task_edit.value && description_task_edit.value) {
+        let index_res = tasks.findIndex((e) => e.id == res.id);
+        tasks[index_res] = {
+          id: res.id,
+          title: title_task_edit.value,
+          description: description_task_edit.value,
+          complete: res.complete,
+        };
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+        print_tasks();
+        task_detail_dialog.close();
+      } else {
+        alert("Complete todos los campos");
+      }
+    });
+  }
+};
+
+const delete_data_by_id = (id) => {
+  let res_index = tasks.findIndex((e) => "delete-" + e.id == id);
+  tasks.splice(res_index, 1);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  print_tasks();
+};
+
 save_btn.addEventListener("click", () => {
   set_data_localstorage();
 });
@@ -73,6 +156,12 @@ list_container.addEventListener("change", (event) => {
 list_container.addEventListener("click", (event) => {
   if (event.target.matches(".detail-btn")) {
     task_detail_dialog.showModal();
+    view_data(event.target.id);
+  } else if (event.target.matches(".edit-btn")) {
+    task_detail_dialog.showModal();
+    generate_form(event.target.id);
+  } else if (event.target.matches(".delete-btn")) {
+    delete_data_by_id(event.target.id);
   }
 });
 close_dialog.addEventListener("click", () => {
